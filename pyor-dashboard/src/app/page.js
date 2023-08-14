e948f19eeffb3bc7a9b1e535b199926b52d5261d
9802HEMENSAN <hmahilange9802@gmail.com>
2023-08-14 13:49:32 +0530

    readme changes

--- pyor-dashboard/src/app/page.js
+++ pyor-dashboard/src/app/page.js
@@ -1,12 +1,22 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MultipleSelectChip from '@/components/MultiSelect';
 import DashBoard from '@/Pages/Dashboard';
-
+const localData=()=>{
+  if(window.localStorage){
+    let data = localStorage.getItem('options');
+    if(data){
+      return JSON.parse(data);
+    }
+    return null;
+  }
+}
 export default function Home() {
-  const [BitCoins, setBitCoins] = useState([])
+  const [BitCoins, setBitCoins] = useState(localData()||[])
   console.log(BitCoins);
-
+useEffect(()=>{
+  localStorage.setItem('options', JSON.stringify(BitCoins));
+},[BitCoins.length])
   return (
     <main style={{ backgroundColor : "#04143d", minHeight: "100vh" , color : "white"}}>
         <MultipleSelectChip BitCoins={BitCoins} setBitCoins={setBitCoins}/>
